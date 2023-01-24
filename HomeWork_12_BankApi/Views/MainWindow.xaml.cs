fb00d36fbce6d1bf702f10674b38973249bdfb0f
6N2P <umnik1985@mail.ru>
2023-01-24 11:14:30 +0500

    Создал таблицы в БД

--- HomeWork_12_BankApi/Views/MainWindow.xaml.cs
+++ HomeWork_12_BankApi/Views/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HomeWork_12_BankApi
+namespace HomeWork_12_BankApi.Views
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
