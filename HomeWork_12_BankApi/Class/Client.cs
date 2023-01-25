894d8aaa67688f455a0c23e06b81fbdc03c869f3
6N2P <umnik1985@mail.ru>
2023-01-25 11:01:36 +0500

    Revert "Настроил ентети"
    This reverts commit a6ee9ceda7d80d09b37b43ccdf962c3fc3ab48f1.

--- HomeWork_12_BankApi/Class/Client.cs
+++ HomeWork_12_BankApi/Class/Client.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace HomeWork_12_BankApi.Class
 {
-    internal class Client
+    public class Client
     {
+        public int ID { get; set; }
+        /// <summary>
+        /// Имя клиента
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Отчество клиента
+        /// </summary>
+        public string Patronomic { get; set; }
+        /// <summary>
+        /// Фамилия клиента
+        /// </summary>
+        public string Surname { get; set; }
+        /// <summary>
+        /// Дата рождения клиента
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+        /// <summary>
+        /// Номер телефона клиента
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// Статус вип клиент или ещё какой
+        /// </summary>
+        public int Status { get; set; }
+
+        public Client() { }
+        public Client( string name, string patronomic, string surname, DateTime dateOfBirth, string phoneNumber, int status)
+        {
+            
+            Name = name;
+            Patronomic = patronomic;
+            Surname = surname;
+            DateOfBirth = dateOfBirth;
+            PhoneNumber = phoneNumber;
+            Status = status;
+        }
     }
 }
